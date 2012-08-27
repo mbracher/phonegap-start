5e0235423a8939b9bb522cc338c7351782d0b0e0
Marc Bracher <m.bracher@mac.com>
2012-08-27 10:42:40 +0200

    added include phonegap and enabled device stuff.

--- assets/js/dsl.js
+++ assets/js/dsl.js
@@ -1,12 +1,16 @@
 // the app method accepts a fn to invoke on init unobtrusively 
 var run = function(application) {
-    if (navigator.userAgent.indexOf('Browzr') > -1) {
-        // blackberry
-        setTimeout(application, 250);
-    } else {
-       // attach to deviceready event, which is fired when phonegap is all good to go.
-       x$(document).on('deviceready', application, false);
-    }
+	
+	var init = function(){
+	
+	    if (navigator.userAgent.indexOf('Browzr') > -1) {
+	        // blackberry
+	        setTimeout(application, 250);
+	    } else {
+	       // attach to deviceready event, which is fired when phonegap is all good to go.
+	       x$(document).on('deviceready', application, false);
+	    };
+		}();
 }
 
 // throw our settings into a lawnchair
