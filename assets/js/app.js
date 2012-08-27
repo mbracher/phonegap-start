badc68194501477c92c3c08e515324338d3ce0a0
Marc Bracher <m.bracher@mac.com>
2012-08-27 10:56:28 +0200

    remover debug alerts

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,25 +3,21 @@
 //
 run(function () {
     // immediately invoked on first run
-alert("step 0");    
    if (navigator.network.connection.type == Connection.NONE) {
         alert("No internet connection - we won't be able to show you any maps or stocks");
     } else {
         alert("We can reach the Interwebz - get ready for some awesome maps and real-time goodness!");
     }
   
-alert("step 0b");
    
     var lsClient = new LightstreamerClient("http://push.lightstreamer.com","DEMO");
-alert("step 0c");
     lsClient.addListener({
       onStatusChange: function(newStatus) {
         x$("#connection_status").html(newStatus);
       }
     });
-alert("step 1");
     lsClient.connect();
-alert("step 2");
+
     var grid = new DynaGrid("stocks",true);
     grid.setSort("stock_name");
     grid.addListener({
@@ -35,7 +31,7 @@ alert("step 2");
         info.setAttribute("white", "black", "color");
       }
     });
-alert("step 3");    
+
     var sub = new Subscription("MERGE",["item3","item4","item5","item6","item7"],grid.extractFieldList()); 
     sub.addListener(grid);
     sub.setDataAdapter("QUOTE_ADAPTER");
@@ -43,7 +39,6 @@ alert("step 3");
 
     lsClient.subscribe(sub);
     
-alert("step 4");    
 
     // a little inline controller
     when('welcome');
